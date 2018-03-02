e6873eac6dd09db353fa35bf65bb6588707f2b06
crystalize50 <36617354+crystalize50@users.noreply.github.com>
2018-03-02 04:17:30 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,6 +9,8 @@ client.on('message', message => {
     if (message.content === 'Ping') {
     	message.channel.send('PONG!');
   	}
+});
+
  client.on('message', message => {
     if (message.content === 'ping') {
     	message.channel.send('PONG!');
@@ -19,6 +21,8 @@ client.on('message', message => {
     if (message.content === 'Bing') {
     	message.reply('BONG!');
   	}
+});
+
 client.on('message', message => {
     if (message.content === 'bing') {
     	message.reply('BONG!');

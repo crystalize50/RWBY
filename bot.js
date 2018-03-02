112567d5c3d174e88422c86c535741d50c65e67e
crystalize50 <36617354+crystalize50@users.noreply.github.com>
2018-03-02 05:00:03 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -100,8 +100,6 @@ client.on('message', message => {
     	message.reply('What's up!');
     }
 });
-//----------------------------------------------------------------------------------------------------------------------------
-
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
